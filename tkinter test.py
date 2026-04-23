0995184f9d6b2a20a810bb083cdaf6fe5e33ad09
Maxim <max-gruntov@mail.ru>
2026-04-23 15:47:35 +0700

    test: add buttons

--- tkinter test.py
+++ tkinter test.py
@@ -1,10 +1,23 @@
-from tkinter import *
- 
-root = Tk()
-root.title("Guess Number")
-root.geometry("500x500")
- 
-label = Label(text="Hello World! This is a Test!")
-label.pack()
- 
-root.mainloop()
+import tkinter as tk
+
+win = tk.Tk()
+
+win.title("Guess Number")
+win.geometry("500x500")
+win.configure(bg = 'lightblue')
+win.resizable(False, False)
+
+label = tk.Label(win, text = "Hello World! This is a Test!")
+label.pack(pady = 20)
+
+def on_button_click():
+    label.config(text = "Кнопка нажата!")
+
+btn = tk.Button(win, text = "Test me", bg = 'white', fg = 'black', command = on_button_click)
+btn.pack(expand = True)
+
+btnclose = tk.Button(win, text = "Close", bg = "red", fg = "black", command = win.destroy)
+btnclose.place(x = 230, y = 350)
+
+win.mainloop()
+
