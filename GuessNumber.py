d34f6fb7271a957106466ddfb24f3a0e424521c2
Maxim <max-gruntov@mail.ru>
2026-04-23 16:12:32 +0700

    style: edit language on English

--- GuessNumber.py
+++ GuessNumber.py
@@ -4,37 +4,37 @@ import random
 def start_game():
     num = random.randint(1, 10)
     attempts = 0
-    print("Я загадал число от 1 до 10. Попробуй угадать")
+    print("I have a number from 1 to 10. Try to guess it")
     
     while True:
         try:
-            guess = int(input("Введите число: "))
+            guess = int(input("Enter a number: "))
         except ValueError:
-            print("Ошибка! Введите число!")
+            print("Error! Enter the number!")
             continue
         if guess < num:
-            print("Ваше число меньше, чем задумано")
+            print("Your number is less than intended")
             attempts += 1
         elif guess > num:
-            print("Ваше число больше, чем задумано")
+            print("Your number is higher than intended")
             attempts += 1
         else:
-            print("Вы угадали!")
-            print("Количество попыток:", attempts + 1)
+            print("You guessed right!")
+            print("Number of attempts:", attempts + 1)
             break
     game(True)
 
 
 def game(retry: bool = False):
-    text = "Угадаете целое число от 1 до 10? (Да/Нет): " if not retry else "Хотите продолжить игру? (Да/Нет):"
+    text = "Can you guess a whole number between 1 and 10? (Yes/No): " if not retry else "Do you want to continue the game? (Yes/No):"
     start = input(text)
     
-    while start.lower() not in ["да", "нет"]:
-        start = input("Пожалуйста, ответьте 'Да' или 'Нет': ")
+    while start.lower() not in ["yes", "no"]:
+        start = input("Please answer 'Yes' or 'No': ")
     
-    if start.lower() == "да":
+    if start.lower() == "yes":
         start_game()
     else:
-        print("До свидания!")
+        print("Goodbye!")
 
 game()
